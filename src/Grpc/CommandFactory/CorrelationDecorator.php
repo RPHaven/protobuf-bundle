bfa9504dd94d5575fea7e2c7fd3067f94c6649ad
Barney Hanlon <barney@shrikeh.net>
2024-05-08 16:49:56 +0100

    Remove auto decoration

--- src/Grpc/CommandFactory/CorrelationDecorator.php
+++ src/Grpc/CommandFactory/CorrelationDecorator.php
@@ -11,15 +11,8 @@ use RpHaven\Protobuf\Grpc\CommandFactory;
 use RpHaven\Protobuf\Grpc\CommandFactory\Exception\UnableToCreateCorrelation;
 use RpHaven\Protobuf\Grpc\Hydration\CorrelationFactory;
 use Spiral\RoadRunner\GRPC\ContextInterface;
-use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 use Symfony\Component\DependencyInjection\Attribute\AutowireDecorated;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
-#[AsDecorator(
-    decorates: CommandFactory::class,
-    priority: 5,
-    onInvalid: ContainerInterface::IGNORE_ON_INVALID_REFERENCE,
-)]
 final readonly class CorrelationDecorator implements CommandFactory
 {
 
